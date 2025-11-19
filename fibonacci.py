95f6618c2125c1144abbbd1a8a400b62ae13a0c2
Göksu <111267914+goksucaglar@users.noreply.github.com>
2025-11-20 00:46:05 +0300

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -97,7 +97,8 @@ for i in range(n-2): #n-2 çünkü 2 sayı zaten hazır -onceki ve suanki-
   onceki = suanki #ikinci sayı birincinin yerine geçer
   suanki = yeni #toplam sonucu şuanki yerine geçer
 
-Bitiş 20.11.2025, 00.45
 #Döngü ile adım adım hesaplıyor, fonksiyon kendini çağırmıyor.
 #Sadece 3 değişken ile ilerliyor, hafıza kullanımı çok düşük, büyük n için çok hızlı.
 #Performans: Döngü (iteratif) yöntem genellikle daha hızlı ve güvenli, büyük sayılar için tercih edilir.
+
+Bitiş 20.11.2025, 00.45
