fecb8a6b955bf28bfb7cd4f9781ca68db18acf8f
Göksu <111267914+goksucaglar@users.noreply.github.com>
2025-11-22 22:55:03 +0300

    Update class.py

--- class.py
+++ class.py
@@ -76,9 +76,16 @@ if secim == "1":
   top.basket_ol()
   
 elif secim == "2":
-  
+  agirlik = float(input("Ağırlık: "))
+  sayi = int(input("Sayı: "))
+  top = VoleybolTopu(renk, marka, agirlik, sayi)
+  top.sayi_kazan()
 
 elif secim == "3":
+  agirlik = float(input("Ağırlık: "))
+  gol_sayisi = int(input("Gol Sayısı: "))
+  top = FutbolTopu(renk, marka, agirlik, gol_sayisi)
+  top.gol_ol()
 
 else:
   print("Geçersiz seçim.")
