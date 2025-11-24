976472fdb35f70f67650015ccac9f1c67d30dd39
Göksu <111267914+goksucaglar@users.noreply.github.com>
2025-11-24 23:23:56 +0300

    Update wasd.py

--- wasd.py
+++ wasd.py
@@ -1,2 +1,20 @@
 # @ işaretini W, A, S, D ile hareket ettirme.
+# import keyboard: klavyenin tüm kontrolünü ele geçiren bir (modül) kütüphanedir.
+# Bu modülle istediğinizi yazabilir, kısayol tuşları oluşturabilir, kısaltmalar oluşturabilir, klavyeyi engelleyebilir, giriş bekleyebilir vb.
+# keyboard.write(message, [delay]) - gecikmeli veya gecikmesiz bir mesaj yazar.
+# keyboard.wait(key) - key tuşuna basılana kadar programı engeller. keyTuş, string ( 'space', 'esc' vb. ) olarak geçirilir. Bir tuşa basılmasını bekleme.   (*)
+# keyboard.press(key )- Bir tuşa basar ve fonksiyon çağrılana kadar basılı tutar. release(key)
+# keyboard.release(key) - bir anahtar serbest bırakır.
+# keyboard.send(key) - bir tuşa basar ve bırakır.
+# keyboard.add_hotkey(hotkey, function) - hotkeybasıldığında bir tane çalıştıran bir tane oluşturur function.    (*)
+# keyboard.record(key)key - tuşuna basılana kadar klavye etkinliğini kaydeder.
+# keyboard.play(recorded_events, [speed_factor]) - fonksiyonu ile kaydedilen olayları keyboard.record(key)isteğe bağlı olarak tekrar oynatır speed_factor.
+# while True: Python’da sonsuz döngü oluşturur. İçine ne koyarsan program onu durmadan çalıştırır.
+# Sonsuz döngüler CPU’yu yorabilir. Döngüye bir çıkış koşulu eklemek genelde daha iyidir.
+#
+
+
+
+
+
 
