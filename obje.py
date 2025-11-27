65c1ce77ebf487ac8dd614c2a4e4d30b3f77b9d0
Göksu <111267914+goksucaglar@users.noreply.github.com>
2025-11-27 23:46:50 +0300

    Update obje.py

--- obje.py
+++ obje.py
@@ -43,7 +43,7 @@ food = Food()
 
 # 6
 alan = [[0 for _ in range(600)] for _ in range(600)] # içteki kod sütun, dıştaki satır, baştaki tüm alan 0 yani boş, 600x600 alan
-alan[kare.y][kare.x] = 1 # 1 = obje, obje alan içine kondu
+# alan[kare.y][kare.x] = 1 # 1 = obje, obje alan içine kondu
 
 # alan = [[0,0,0,0,0],  # alan[0] → 0. satır
         # [0,0,0,0,0],  # alan[1] → 1. satır
@@ -91,6 +91,78 @@ if yılan[0].x == food.x and yılan[0].y == food.y:
 # hız
 time.sleep(0.1)
 
+# TEMİZ KOD ---------------------------------------------------------------------------------------------------------------------
 
+# 1
+import os
+import time
+import random
+import keyboard
+
+# 2
+class Snake:
+  def __init__(self, x, y):
+    self.x = x
+    self.y = y
+    self.dx = 1 # başlangıç yönü
+    self.dy = 0
+
+  def hareket(self):
+    self.x += self.dx
+    self.y += self.dy
+
+# 3
+class Food():
+  def __init__(self):
+    self.x, self.y = self.yeni_yem()
+    
+  def yeni_yem(self):
+    self.x = random.randint(0,599)
+    self.y = random.randint(0,599)
+    return self.x, self.y
+
+# 4
+yılan = [Snake(50, 50), # ilk parça head
+         Snake(49,50), 
+         Snake(48,50)
+] 
+
+# 5
+food = Food()
+
+# 6
+alan = [[0 for _ in range(600)] for _ in range(600)] 
+# içteki kod sütun, dıştaki satır, baştaki tüm alan 0 yani boş, 600x600 alan
+
+# 7
+while True:
+  if keyboard.is_pressed("w"):
+    yılan[0].dx = 0
+    yılan[0].dy = -1
+  elif keyboard.is_pressed("a"):
+    yılan[0].dx = -1
+    yılan[0].dy = 0
+  elif keyboard.is_pressed("s"):
+    yılan[0].dx = 0
+    yılan[0].dy = 1
+  elif keyboard.is_pressed("d"):
+    yılan[0].dx = 1
+    yılan[0].dy = 0
+    
+# Gövdenin takip etmesi
+for i in range(len(yılan)-1, 0, -1): # range(start, stop, step) , 2,0,-1 -> 2,1 
+  yılan[i].x = yılan[i-1].x # i = 2 → S2, S1’in eski pozisyonunu alır
+  yılan[i].y = yılan[i-1].y # i = 1 → S1, Head’in eski pozisyonunu alır
+
+# headi hareket ettirme
+yılan[0].hareket()
+
+# yem yendi mi?
+if yılan[0].x == food.x and yılan[0].y == food.y:
+  yılan.append(Snake(yılan[-1].x, yılan[-1].y)) # büyüme
+  food.x, food.y = food.yeni_yem() # yem yeni rastgele konuma taşınır.
+
+# hız
+time.sleep(0.1)
 
 
