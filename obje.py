f400e1379897476f5067040d125484d6c1ca9189
Göksu <111267914+goksucaglar@users.noreply.github.com>
2025-11-27 19:20:52 +0300

    Create obje.py

---
+++ obje.py
@@ -0,0 +1,4 @@
+# Haraket ettirdigin cismi obje olarak tanimla kendisi saniyede 1 jere en son
+# yonlendirilen yone haraket etsin 600x600 bi console da, 1 tane yem olsun onu 
+# alinca cisim buyusun ve yeni yem viksin bunlari flasslarda tut dinamik bi sekilde 
+# ohjemiz buyusun ve her bir parcasinin konumu dinamik olarak yerini tutsun
