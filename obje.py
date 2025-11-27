6d2b04561c1f3cef579dc3f57fc028134490b5ef
Göksu <111267914+goksucaglar@users.noreply.github.com>
2025-11-27 19:40:24 +0300

    Update obje.py

--- obje.py
+++ obje.py
@@ -1,4 +1,5 @@
-# Haraket ettirdigin cismi obje olarak tanimla kendisi saniyede 1 jere en son
-# yonlendirilen yone haraket etsin 600x600 bi console da, 1 tane yem olsun onu 
-# alinca cisim buyusun ve yeni yem viksin bunlari flasslarda tut dinamik bi sekilde 
-# ohjemiz buyusun ve her bir parcasinin konumu dinamik olarak yerini tutsun
+# Haraket ettirdigin cismi obje olarak tanimla 
+# kendisi saniyede 1 kere en son yönlendirilen yöne hareket etsin 
+# 600x600 bi console da, 1 tane yem olsun onu  alinca cisim buyusun 
+# ve yeni yem çıksın bunlari classlarda tut dinamik bir şekilde 
+# objemiz büyüsün ve her bir parçasının konumu dinamik olarak yerini tutsun
