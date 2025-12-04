5f65ad2876cf3f66c0ba4b4c0025bf28af91fb30
Göksu <111267914+goksucaglar@users.noreply.github.com>
2025-12-04 18:32:33 +0300

    Create robot.py

---
+++ robot.py
@@ -0,0 +1,12 @@
+# "Robot Koloni Simülasyonu - Pathfinding
+# + Strategy Al"
+# Ödev Özeti
+# Bir 2D harita üzerinde yaşayan
+# robotlardan oluşan bir koloni simüle edeceksin.
+# Robotlar:
+# Haritada dolaşacak
+# Engellerden kaçacak
+# Kaynaklar toplayacak
+# Bir strateji algoritmasıyla karar verecek
+# Pathfinding (A*, BFS veya Dijkstra zorunlu) kullanacak
+# Tüm sistem OOP ile tasarlanacak.
